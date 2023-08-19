d41b0bb463a95abf0e176f45f1e0fd539db0e577
Avaz <elonarm21@gmail.com>
2023-08-20 02:15:32 +0500

    robofriends project

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="app">
+        <div className="container">
+          <h1 className="tc">welcome</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
